db80dd501ad2d06dd8fa7615d79d5885a007a613
rajesh <rajesh@35779@gmail.com>
2024-03-23 09:44:06 +0530

    ok

---
+++ ArraylistDemo.java
@@ -0,0 +1,15 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+    public static void main(String[] args) {
+        List list=new ArrayList<>();
+
+        list.add(123);
+        list.add("rajesh");
+        list.add("QA");
+
+        System.out.println(list);
+    }
+}
